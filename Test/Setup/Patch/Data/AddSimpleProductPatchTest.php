7523eccb4bd2f5d138d8df6d97dc12726d96e056
Ian Ramos <ian.ramos@scandiweb.com>
2022-09-08 02:24:11 -0300

    Adjustments post code review

--- Test/Setup/Patch/Data/AddSimpleProductPatchTest.php
+++ Test/Setup/Patch/Data/AddSimpleProductPatchTest.php
@@ -1,101 +1,155 @@
 <?php
 
+/**
+ * @category  Scandiweb
+ * @author    Ian Ramos <ian.ramos@scandiweb.com>
+ * @license   http://opensource.org/licenses/OSL-3.0 The Open Software License 3.0 (OSL-3.0)
+ * @copyright Copyright (c) 2022 Scandiweb, Inc (https://scandiweb.com)
+ *
+ */
+
 namespace Scandiweb\Test\Setup\Patch\Data;
 
+use Magento\Catalog\Api\CategoryLinkManagementInterface;
 use Magento\Catalog\Api\Data\ProductInterfaceFactory;
 use Magento\Catalog\Api\ProductRepositoryInterface;
-use Magento\Framework\Setup\ModuleDataSetupInterface;
-use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Catalog\Model\Product;
+use Magento\Catalog\Model\Product\Attribute\Source\Status;
+use Magento\Catalog\Model\Product\Type;
+use Magento\Catalog\Model\Product\Visibility;
+use Magento\InventoryApi\Api\Data\SourceItemInterface;
+use Magento\InventoryApi\Api\Data\SourceItemInterfaceFactory;
+use Magento\InventoryApi\Api\SourceItemsSaveInterface;
 use Magento\Store\Model\StoreManagerInterface;
-use Magento\Catalog\Model\CategoryFactory;
-use Magento\Catalog\Api\CategoryRepositoryInterface;
+use Magento\Eav\Setup\EavSetup;
 use Magento\Framework\App\State;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
 
 class AddSimpleProductPatchTest implements DataPatchInterface
 {
     /**
+     * @var ProductInterfaceFactory
+     */
+    protected ProductInterfaceFactory $productFactory;
+
+    /**
      * @var ProductRepositoryInterface
      */
-    protected $productRepository;
-
-    /**
-     * @var CategoryFactory
-     */
-    protected $categoryFactory;
-
-    /**
-     * @var CategoryRepositoryInterface
-     */
-    protected $categoryRepository;
+    protected ProductRepositoryInterface $productRepository;
 
     /**
      * @var StoreManagerInterface
      */
-    protected $storeManager;
+    protected StoreManagerInterface $storeManager;
 
     /**
-     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @var State
+     */
+    protected State $appState;
+
+    /**
+     * @var SourceItemInterfaceFactory
+     */
+    protected SourceItemInterfaceFactory $sourceItemFactory;
+
+    /**
+     * @var SourceItemsSaveInterface
+     */
+    protected SourceItemsSaveInterface $sourceItemsSaveInterface;
+
+    /**
+     * @var EavSetup
+     */
+    protected EavSetup $eavSetup;
+
+    /**
+     * @var array
+     */
+    protected array $sourceItems = [];
+
+    /**
+     *
      * @param ProductInterfaceFactory $productFactory
      * @param ProductRepositoryInterface $productRepository
-     * @param CategoryFactory $categoryFactory
-     * @param CategoryRepositoryInterface $categoryRepository
+     * @param SourceItemInterfaceFactory $sourceItemFactory
+     * @param SourceItemsSaveInterface $sourceItemsSaveInterface
+     * @param State $appState
      * @param StoreManagerInterface $storeManager
+     * @param EavSetup $eavSetup
+     * @param CategoryLinkManagementInterface $categoryLink
      */
     public function __construct(
-        ModuleDataSetupInterface $moduleDataSetup,
         ProductInterfaceFactory $productFactory,
         ProductRepositoryInterface $productRepository,
-        CategoryFactory $categoryFactory,
-        CategoryRepositoryInterface $categoryRepository,
+        State $appState,
         StoreManagerInterface $storeManager,
-        State $state
-    )
-    {
-        $this->moduleDataSetup    = $moduleDataSetup;
-        $this->productFactory     = $productFactory;
-        $this->productRepository  = $productRepository;
-        $this->categoryFactory    = $categoryFactory;
-        $this->categoryRepository = $categoryRepository;
-        $this->storeManager       = $storeManager;
-        $state->setAreaCode('adminhtml');
+        EavSetup $eavSetup,
+        SourceItemInterfaceFactory $sourceItemFactory,
+        SourceItemsSaveInterface $sourceItemsSaveInterface,
+        CategoryLinkManagementInterface $categoryLink
+    ) {
+        $this->appState = $appState;
+        $this->productFactory = $productFactory;
+        $this->productRepository = $productRepository;
+        $this->eavSetup = $eavSetup;
+        $this->storeManager = $storeManager;
+        $this->sourceItemFactory = $sourceItemFactory;
+        $this->sourceItemsSaveInterface = $sourceItemsSaveInterface;
+        $this->categoryLink = $categoryLink;
     }
 
-    /**
-     * @return string
-     */
-    public function apply()
+    public function apply(): void
+    {
+        $this->appState->emulateAreaCode('adminhtml', [$this, 'execute']);
+    }
+
+    public function execute(): void
     {
         $product = $this->productFactory->create();
 
-        $product->setSku('123456789')
+        if ($product->getIdBySku('cool-test-product')) {
+            return;
+        }
+
+        $attributeSetId = $this->eavSetup->getAttributeSetId(Product::ENTITY, 'Default');
+        $websiteIDs = [$this->storeManager->getStore()->getWebsiteId()];
+        $product->setTypeId(Type::TYPE_SIMPLE)
+            ->setWebsiteIds($websiteIDs)
+            ->setAttributeSetId($attributeSetId)
             ->setName('Test Product')
-            ->setAttributeSetId('4')
-            ->setStatus(1)
-            ->setWeight(2)
-            ->setPrice(0)
-            ->setVisibility(1)
-            ->setTypeId('simple')
-            ->setStockData(
-                array(
-                    'use_config_manage_stock' => 0,
-                    'manage_stock' => 1,
-                    'is_in_stock' => 1,
-                    'qty' => 1
-                )
-            );
+            ->setUrlKey('test-product')
+            ->setSku('cool-test-product')
+            ->setPrice(1)
+            ->setVisibility(Visibility::VISIBILITY_BOTH)
+            ->setStatus(Status::STATUS_ENABLED)
+            ->setStockData(['use_config_manage_stock' => 1, 'is_qty_decimal' => 0, 'is_in_stock' => 1]);
         $product = $this->productRepository->save($product);
 
-        $product->save();
+        $sourceItem = $this->sourceItemFactory->create();
+        $sourceItem->setSourceCode('default');
+        $sourceItem->setQuantity(99);
+        $sourceItem->setSku($product->getSku());
+        $sourceItem->setStatus(SourceItemInterface::STATUS_IN_STOCK);
+        $this->sourceItems[] = $sourceItem;
+
+        $this->sourceItemsSaveInterface->execute($this->sourceItems);
+
+        $this->categoryLink->assignProductToCategories($product->getSku(), [2]);
     }
 
-    public static function getDependencies()
+    /**
+     * {@inheritDoc}
+     */
+    public static function getDependencies(): array
     {
         return [];
     }
 
-    public function getAliases()
+    /**
+     * {@inheritDoc}
+     */
+    public function getAliases(): array
     {
         return [];
     }
-
 }
